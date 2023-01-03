e97f46352fadf7fe57db6e6d0faaf0a1a173be1c
Jaye <jfranklin773@gmail.com>
2023-01-03 15:36:55 -0800

    Add sendgrid api

--- send-email.php
+++ send-email.php
@@ -1,7 +1,7 @@
 <?php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
+//require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
+ require("sendgrid-php/sendgrid-php.php");
 // If not using Composer, uncomment the above line and
 // download sendgrid-php.zip from the latest release here,
 // replacing <PATH TO> with the path to the sendgrid-php.php file,
